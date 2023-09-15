9cb19947bdb0742d4403c8b3aa33d758d9465007
Luis Prates <lprates@student.42lisboa.com>
2023-09-15 23:27:37 +0100

    Removes gcd() and small clean up

--- src/main.rs
+++ src/main.rs
@@ -202,9 +202,6 @@ fn main() {
 			println!("The equation provided is invalid");
 		}
 	}
-
-
-
 }
 
 fn print_polynomial(coefficients: &[f64]) {
@@ -251,17 +248,4 @@ fn my_sqrt(x: f64) -> f64 {
 	}
 
 	guess
-}
-
-fn gcd(mut n: u64, mut m: u64) -> u64 {
-	assert!(n != 0 && m != 0);
-	while m != 0 {
-		if m < n {
-			let t = m;
-			m = n;
-			n = t;
-		}
-		m = m % n;
-	}
-	n
 }
